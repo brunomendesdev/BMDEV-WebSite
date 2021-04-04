6b2e21f9294b5d0f027537903650ae5230495441
BrunoMendes <brunomends02@outlook.com>
2021-04-04 18:18:24 -0300

    Update 06

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -40,7 +40,7 @@ function createCardFace(face, card, element){
     if(face === FRONT){
         let iconElement = document.createElement('img')
         iconElement.classList.add(ICON)
-        iconElement.src = "./assets/images/"+card.icon+".png"
+        iconElement.src = "./public/images/"+card.icon+".png"
         cardElementFace.appendChild(iconElement)
     }else{
         cardElementFace.innerHTML = "&lt/&gt"
